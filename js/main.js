4d23f6c4c0e9e2dffa9c39f34d30a7fec15a2787
NoLAAG <74335288+NoLAAG@users.noreply.github.com>
2021-07-18 22:48:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,19 @@ inputs.forEach(input =>{
 })
 
 
+const userId = document.getElementById("username");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const database = firebase.database();
+const rootRef = database.ref('accounts');
+
+addBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	rootRef.child(userId.value).set({
+		name=name.value,
+		email=email.value,
+		password=password
+	});
+});
