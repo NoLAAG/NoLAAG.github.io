868116806383d8d65f029b79f408e9ee8f51f39b
NoLAAG <74335288+NoLAAG@users.noreply.github.com>
2021-07-18 14:33:30 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect("localhost","root","","website");
+
+$name= $_POST['T1'];
+$username= $_POST['T2'];
+$email= $_POST['T3'];
+$password= $_POST['T4'];
+
+$req="Select * from Account where username='$username'";
+$res=mysqli_query($con,$req);
+
+if(mysqli_num_rows($res) >= 1){
+	echo "$username is already taken";
+}else {
+	$req="insert into Account(name,username,email,password) Values('$name','$username','$email','$password')";
+	mysqli_query($con,$req);
+	echo"Done!";
+}
+
+?>
